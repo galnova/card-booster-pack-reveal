5d708e34564aa96efda0bd8a6a437f491db948cd
Keith Jeter <galnova@gmail.com>
2026-08-25 22:34:57 -0400

    sydney and card art update

--- js/card-render.js
+++ js/card-render.js
@@ -29,6 +29,7 @@ const CARD_IMAGE_OVERRIDES = {
   "mech-linwood": "card-samples/HS-Card-Mech-Strategist-Linwood.png",
   "mech-gigatear": "card-samples/HS-Card-Mech-Fortifier-Gigatear.png",
   "mech-lasereater": "card-samples/HS-Card-Mech-Arsenal-Lasereater.png",
+  "mech-sydney": "card-samples/HS-Card-Mech-Strategist-Sydney.png",
 };
 
 function scopeClassFor(card, setById) {
